e4f56a4fe93c6009c634cda9b12639352a338f14
dominikabal <74771198+dominikabal@users.noreply.github.com>
2020-11-20 16:10:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,196 @@
+$("#quiz1").dlxQuiz({
+    quizData: {
+        questions: [
+            {
+                q:
+                    'Zgodnie z kodeksem karnym, sprawca czynu zabronionego, jezeli nie mozna mu przypisac winy w czasie czynu:',
+                a: "nie popełnia przestepstwa",
+                options: [
+                    "nie podlega karze",
+                    "popełnia przestępstwo, ale sąd moze odstąpić od wymierzenia kary",
+                    "nie popełnia przestepstwa"
+                ]
+            },
+            {
+                q: "Zgodnie z Kodeksem karnym, orzekając karę pozbawienia wolności bez warunkowego zawieszenia jej wykonania, sąd:",
+                a: "może określić rodzaj i typ zakładu karnego, w którym skazany ma odbywać karę",
+                options: [
+                    "nie może określić rodzaju i typu zakładu karnego, w którym skazany ma odbywać karę",
+                    "zawsze określa rodzaj i typ zakładu karnego, w którym skazany ma odbywać karę",
+                    "może określić rodzaj i typ zakładu karnego, w którym skazany ma odbywać karę"
+                ]
+            },
+            {
+                q: "Zgodnie z Kodeksem karnym, jeżeli skazany po wydaniu wyroku, którym orzeczono karę pozbawienia wolności z warunkowym zawieszeniem jej wykonania, lecz przed jego uprawomocnieniem się, rażąco narusza porządek prawny, a w szczególności gdy w tym czasie popełnił przestępstwo, sąd:",
+                a: "moze zarządzić wykonanie kary",
+                options: ["moze zarządzić wykonanie kary", "zarządza wykonanie kary","zarządza wykonanie kary wyłącznie na wniosek kuratora"]
+            },
+            {
+                q: "Zgodnie z K.K., środkiem zabezpieczającym nie jest:",
+                a: "nawiązka",
+                options: ["nawiązka", "terapia", "elektroniczna kontrola miejsca pobytu"]
+            },
+            {
+                q:
+                    "Zgodnie z Kodeksem karnym, sprawca, wobec którego orzeczono środek zabezpieczający w postaci elektronicznej kontroli miejsca pobytu:",
+                a: "ma obowiązek poddania się nieprzerwanej kontroli miejsca swojego pobytu sprawowanej za pomocą urządzeń technicznych, w tym noszonego nadajnika",
+                options: ["ma obowiązek poddania się nieprzerwanej kontroli miejsca swojego pobytu sprawowanej za pomocą urządzeń technicznych, w tym noszonego nadajnika", "moze poddać się kontroli miejsca swojego pobytu sprawowanej za pomocą urządzeń technicznych, w tym za pomocą noszonego nadajnika", "mozna poddac sie nieprzerywanej kontroli miejsca swojego pobytu sprawowanej za pomoca urzadzen technicznych, wyjątkowo takze za pomoca noszenia nadajnika"]
+            },
+{
+    q:
+    "Zgodnie z Kodeksem karnym, z chwilą zatarcia skazania:",
+    a: "uwaza się je za niebyłe; wpis o skazaniu usuwa się z rejestru skazanych", 
+    
+    options: ["uwaza się je za niebyłe; wpis o skazaniu usuwa się z rejestru skazanych tylko na wniosek skazanego, wyłącznie w wypadkach, gdy ustawa dopuszcza taką moliwość", "uwaza się je za niebyłe; wpis o skazaniu usuwa się z rejestru skazanych", "uwaza się je za niebyłe; wpisu o skazaniu nie usuwa się z rejestru skazanych"]
+
+
+},
+
+{
+
+q: "Zgodnie z Kodeksem karnym, funkcjonariuszem publicznym nie jest:",
+a: "pracownik międzynarodowego trybunału karnego pełniący wyłącznie czynności usługowe",
+
+options: ["pracownik międzynarodowego trybunału karnego pełniący wyłącznie czynności usługowe", "funkcjonariusz Słuzby Więziennej","poseł do Parlamentu Europejskiego"]
+    },
+
+    {
+q: "Zgodnie z Kodeksem karnym, przepisy części ogólnej tego kodeksu:",
+a:"stosuje się do innych ustaw przewidujących odpowiedzialność karną, chyba ze ustawy te wyraźnie wyłączają ich zastosowanie",
+options: ["mozna stosować do innych ustaw przewidujących odpowiedzialność karną wyłącznie wtedy, gdy ustawy te tak stanowią",
+"nigdy nie mają zastosowania do innych ustaw przewidujących odpowiedzialność kanrą",
+"stosuje się do innych ustaw przewidujących odpowiedzialność karną, chyba ze ustawy te wyraźnie wyłączają ich zastosowanie"]
+
+},
+
+{
+
+    q:"Zgodnie z Kodeksem karnym, kto, wiedząc, ze jest zagrozony wirusem HIV, naraza bezpośrednio inną osobę na takie zarazenie, podlega karze pozbawienia wolnosci od 6 miesiecy do lat 8; ściganie tego przestępstwa następuje:",
+    a:"na wniosek pokrzywdzonego",
+    options:["z oskarzenia prywatnego", "niezalenie od tego, czy pokrzywodzny zlozy wniosek o ściganie", "na wniosek pokrzywdzonego"], 
+
+
+
+},
+
+{
+q:"Zgodnie z Kodeksem postępowania karnego, oskarzyciel publiczny moze cofnąc akt oskarzenia bez zgody oskarzonego:",
+a: "do czasu rozpoczęcia przewodu sądowego na pierwszej rozprawie głównej",
+options:["do czasu rozpoczęcia przewodu sądowego na pierwszej rozprawie głównej","w toku przewodu sądowego przed sądem pierwszej instancji","do czasu prawomocnego zakończenia postepowania w sprawie"]
+
+
+},
+
+{
+
+q:"Zgodnie z Kodeksem postępowania karnego, spór o właściwość między sądem apelacyjnym a innym sądem powszechnym rozstrzyga:",
+a:"Sąd Najwyszy",
+options:["Sąd Najwyzszy","sąd apelacyjny właściwy ze względu na siedzibę sądu, który pierwszy wszczął spór","Trybunał Konstytucyjny"]
+
+
+},
+
+{
+  q:"Zgodnie z Kodeksem postępowania karnego, stosowanie w czasie badania przez biegłego środków technicznych mających na celu kontrolę nieświadomych reakcji organizmu badanej osoby:",
+  a:"mozliwe jest wyłącznie za jej zgodą",
+  options:["mozliwe jest wyłącznie za jej zgodą","mozliwe jest nawet bez jej zgody","nie jest mozliwe, nawet za jej zgodą"] 
+
+
+},
+
+{
+
+    q:". Zgodnie z Kodeksem postępowania karnego, gdy w sprawie ujawnią się nowe istotne fakty lub dowody nie znane w poprzednim postępowaniu, prawomocnie umorzone postępowanie przygotowawcze wznawia się przeciwko osobie, która występowała w charakterze podejrzanego, na mocy postanowienia:",
+    a:"prokuratora nadrzędnego nad tym, który wydał lub zatwierdził postanowienie o umrzeniu",
+options:["prokuratora nadrzędnego nad tym, który wydał lub zatwierdził postanowienie o umorzeniu","sądu właściwego do rozpoznania sprawy","sądu apelacyjnego, w obszarze którego zostało popełnione przestępstwo"]
+
+},
+{
+q:"Zgodnie z Kodeksem postępowania karnego, zazalenie przysługuje na postanowienie sądu:",
+a:"co do środka zabezpieczjącego",
+options:["o oddaleniu wniosku dowodowego","w przedmiocie wyłączenia sędziego","co do rodka zabezpieczającego"]
+
+
+},
+
+{
+    q:"Zgodnie z Kodeksem pos†ępowania karnego, wydanie wyroku nakazowego jest niedopuszczalne:",
+    a:"w sprawie z oskarzenia prywatnego",
+    options:["w stosunku do osoby pozbawionej wolności w tej lub innej sprawie","w sprawie z oskarzenia rywatnego","jezeli zachodzą warunki do orzeczenia kary ograniczenia wolności w stosunku do oskarzonego"]
+
+},
+
+{
+    q:"Zgodnie z Kodeksem postępowania karnego, jeżeli Sąd Najwyższy po rozpoznaniu kasacji uznał ją za oczywiście bezzasadną, wówczas:",
+    a:"postanowieniem oddala kasację",
+    options:["wyrokiem oddala kasację","postanowieniem oddala kasację","postanowieniem pozostawia kasację bez rozpoznania"]
+
+},
+
+{
+q:"Zgodnie z Kodeksem postępowania karnego, jeżeli zachodzą warunki do orzeczenia kary łącznej w stosunku do osoby prawomocnie skazanej wyrokami róznych sądów i zachodzi zbieg wyroków sądu powszechnego i szczególnego, o karze łącznej orzeka ten z sądów, który:",
+a:"wymierzył karę surowszą podlegającą łączeniu",
+options:["wymierzył karę surowszą podlegającą łączeniu","wydał pierwszy wyrok skazujący w pierwszej instancji, orzekając kary podlegające łączeniu", "wydał ostatni wyrok skazujący w pierwszej instancji, orzekający kary podlegające łączeniu"]
+
+
+},
+
+{
+q:" Zgodnie z Kodeksem postępowania karnego, na postanowienie sądu okręgowego w przedmiocie wystapienia do właściwego sądu lub innego organu państwa członkowskiego Unii Europejskiej o wykonanie w innym państwie członkowskim Unii Europejskiej prawomocnie orzeczonej przez sąd polski wobec obywatela polskiego lub cudzoziemca kary pozbawienia wolności podlegającej wykonaniu:",
+a:"nie przysługuje zazalenie",
+options:["nie przysługuje zazalenie","przysługuje zazalenie do sądu apelacyjnego","przysługuje zazalenie do innego równorzędnego składu sądu okręgowego"]
+
+},
+
+{
+q:"Zgodnie z Kodeksem wykroczeń, wykroczenie mozna popełnić:",
+a:"zarówno umyślnie, jak i nieumyślnie, chyba ze ustawa przewiduje odpowiedzialność tylko za wykroczenie umyślne",
+options:["w kazdym wypadku tylko umyślnie","w kazdym wypadku zarówno umyślnie, jak i nieumyślnie","zarówno umyślnie, jak i nieumyślnie, chyba ze ustawa przewiduje odpowiedzialność tylko za wykroczenie umyślne"]
+
+},
+{
+    q:"Zgodnie z Kodeksem wykroczeń, karę za podzeganie lub za pomocnictwo wymierza się:",
+    a:"w granicach zagrozenia przewidzianego dla danego wykroczenia",
+    options:["w granicach zagrozenia przewidzianego dla danego wykroczenia","od jednej trzeciej dolnej granicy ustawowego zagrozenia przewidzianego dla danego wykroczenia od górnej granicy tego zagrozenia","od jednej drugiej dolnej granicy ustawowego zagrozenia przewidzianego dla danego wykroczenia do górnej granicy tego zagrozenia"]
+},
+
+{
+q:"Zgodnie z Kodeksem wykroczeń, kara aresztu trwa:",
+a:"najkrócej 5, najdłuzej 30 dni",
+options:["najkrócej 10, najdłuzej 60 dni","najkrócej 5, najdłuzej 30 dni","najkrócej 3 dni, najdłuzej 45 dni"]
+
+},
+
+{
+q:" Zgodnie z Kodeksem wykroczeń, w czasie odbywania kary ograniczenia wolności ukarany:",
+a:"ma obowiązek udzielania wyjaśnień dotyczących przebiegu odbywania kary",
+options:[" nie ma obowiązku udzielania wyjaśnień dotyczących przebiegu odbywania kary",
+" ma obowiązek udzielania wyjaśnień dotyczących przebiegu odbywania kary",
+"ma obowiązek udzielania wyjaśnień dotyczących przebiegu odbywania kary jedynie na ządanie kuratora"]
+},
+{
+q:"Zgodnie z Kodeksem wykroczeń, ukaranemu co najmniej dwukrotnie za podobne wykroczenia umyślne, który w ciągu dwóch lat od ostatniego ukarania popełnia ponownie podobne wykroczenie umyślne, można wymierzyć:",
+a:"karę aresztu, choćby wykroczenie to było zagrozone karą łagodniejszą",
+options:[" karę grzywny lub karę ograniczenia wolności w wymiarze do górnej granicy ustawowego zagrozenia podwyzszonej o połowę",
+"karę nagany obok orzeczonej kary grzywny, kary ograniczenia wolności lub kary aresztu",
+"karę aresztu, choćby wykroczenie to było zagrozone karą łagodniejszą"]
+},
+{
+
+q:" Zgodnie z Kodeksem postępowania w sprawach o wykroczenia, jeżeli oskarzyciel publiczny nie bierze udziału w rozprawie, wniosek o ukaranie:",
+a:"odczytuje protokolant",
+options:[" nie podlega odczytaniu",
+"odczytuje sędzia",
+"odczytuje protokolant"]
+},
+
+
+
+
+    
+
+
+
+        ]
+    }
+});
